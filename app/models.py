bedf8a528f9236b8f77461f03661fd43ecaedf00
omar elattar <omarelattar2002@gmail.com>
2024-03-30 06:34:34 -0500

    Hw2

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from . import db
+from datetime import datetime, timezone
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    due_date = db.Column(db.DateTime)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import request
 from app import app
 from fake_tasks.tasks import tasks_list
+from datetime import datetime
 
 
 @app.route("/")
@@ -18,4 +19,32 @@ def get_tasks(task_id):
         if task['id'] == task_id:
             return task
     return {'error':f'The ID{task_id} does not exist'}, 404
-    
+
+
+@app.route("/tasks", methods=['POST'])
+def create_task():
+    if not request.is_json:
+        return {'error':'Your content must be json'}, 400
+    data = request.json
+    required_fields = ['title', 'description']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error':f'{', '.join(missing_fields)} must be present'}
+    title = data['title']
+    description = data['description']
+    new_task = {
+        "id": len(tasks_list) + 1,
+        "title": title,
+        "description": description,
+        "completed": False,
+        "createdat": '24/12/2024',
+        "duedate": data.get('duedate')
+    }
+
+    tasks_list.append(new_task)
+
+
+    return new_task, 201
