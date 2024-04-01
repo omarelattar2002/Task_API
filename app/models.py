90865a340b145607867fc0561f67e6ab7b1a0e86
omar elattar <omarelattar2002@gmail.com>
2024-04-01 04:07:13 -0500

    final commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
+import secrets
 from . import db
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +9,56 @@ class Task(db.Model):
     completed = db.Column(db.Boolean, nullable=False, default=False)
     created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     due_date = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', back_populates='tasks')
+
+
+    @property
+    def to_json(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'completed': self.completed,
+            'created_at': self.created_at,
+            'due_date': self.due_date,
+        }
+    
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(120), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    token = db.Column(db.String, index=True, unique=True, default="default")
+    token_expiration = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc) + timedelta(minutes=5))
+    tasks = db.relationship('Task', back_populates='author')
+
+    def __init__(self,**kwargs):
+        super(User, self).__init__(**kwargs)
+        self.get_token()
+
+    def get_token(self):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return {"token": self.token, "token_expiration": self.token_expiration}
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(minutes=5)
+        db.session.add(self)
+        db.session.commit()
+        return {"token": self.token, "token_expiration": self.token_expiration}
+
+    def check_password(self, password):
+        return hash(password) == self.password_hash
+
+
+    @property
+    def to_json(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'date_created': self.date_created,
+        }
+
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import request
 from app import app
+from app.models import Task, User
 from fake_tasks.tasks import tasks_list
 from datetime import datetime
-
+from . import db
+from flask import jsonify
+from app.auth import basic_auth, token_auth
 
 @app.route("/")
 def index():
@@ -10,18 +13,20 @@ def index():
 
 @app.route("/tasks")
 def show_tasks():
-    return tasks_list
+    return jsonify(json_list=[task.to_json for task in Task.query.all()])
+
 
 
 @app.route("/tasks/<int:task_id>")
 def get_tasks(task_id):
-    for task in tasks_list:
-        if task['id'] == task_id:
-            return task
-    return {'error':f'The ID{task_id} does not exist'}, 404
+    task = Task.query.get(task_id)
+    if not task:
+        return {'error':f'The task with ID {task_id} does not exist'}, 404
+    return jsonify(task.to_json), 200
 
 
 @app.route("/tasks", methods=['POST'])
+@token_auth.login_required
 def create_task():
     if not request.is_json:
         return {'error':'Your content must be json'}, 400
@@ -35,16 +40,78 @@ def create_task():
         return {'error':f'{', '.join(missing_fields)} must be present'}
     title = data['title']
     description = data['description']
-    new_task = {
-        "id": len(tasks_list) + 1,
-        "title": title,
-        "description": description,
-        "completed": False,
-        "createdat": '24/12/2024',
-        "duedate": data.get('duedate')
-    }
+    current_user = token_auth.current_user()
+    new_task = Task(title=title, description=description, user_id=current_user.id)
+    db.session.add(new_task)
+    db.session.commit()
+    return new_task.to_json, 201
 
-    tasks_list.append(new_task)
+@app.route("/tasks/<int:task_id>", methods=['PUT'])
+@token_auth.login_required
+def update_task(task_id):
+    Task.query.filter_by(id=task_id).update({'completed': True})
+    db.session.commit()
+    return {'message':f'Task {task_id} has been completed'}, 200
 
 
-    return new_task, 201
+@app.route("/tasks/<int:task_id>", methods=['DELETE'])
+@token_auth.login_required
+def delete_task(task_id):
+    Task.query.filter_by(id=task_id).delete()
+    db.session.commit()
+    return {'message':f'Task {task_id} has been deleted'}, 200
+
+
+@app.route("/users", methods=['POST'])
+def create_user():
+    if not request.is_json:
+        return {'error':'Your content must be json'}, 400
+    data = request.json
+    required_fields = ['username', 'password']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error':f'{', '.join(missing_fields)} must be present'}, 400
+    username = data['username']
+    email = data['email']
+    password = data['password']
+    new_user = User(username=username, email=email, password_hash=hash(password))
+    return new_user.to_json, 201
+
+
+@app.route("/users/<int:user_id>")
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return {'error':f'The user with ID {user_id} does not exist'}, 404
+    return jsonify(user.to_json), 200
+
+@app.route("/users/<int:user_id>", methods=['PUT'])
+@token_auth.login_required
+def update_user(user_id):
+    if not request.is_json:
+        return {'error':'Your content must be json'}, 400
+    user = User.query.get(user_id)
+    if not user:
+        return {'error':f'The user with ID {user_id} does not exist'}, 404
+    return user
+    
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return {'error':f'The user with ID {user_id} does not exist'}, 404
+    db.session.delete(user)
+    db.session.commit()
+    return {'message':f'User {user_id} has been deleted'}, 200
+
+
+@app.route('/token')
+@basic_auth.login_required
+def create_token():
+    user = basic_auth.current_user()
+    token = user.get_token()
+    return jsonify({'token': token.get("token").decode('utf-8'),"token-expiration":token.get("token_expiration")}), 200
